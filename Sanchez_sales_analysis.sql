eea41058ed444947eadb814155f300ef45e13155
dahlia sanchez <dsanchez1@my.yearupunited.org>
2026-04-28 09:06:53 -0700

    updated Sanchez_sales_analysis

--- Sanchez_sales_analysis.sql
+++ Sanchez_sales_analysis.sql
@@ -3,16 +3,68 @@
 use sample_sales;
 
 select * from management; -- To see what territory i am analyzing; South Carolina
-select * from store_locations; --  Find my terriory storeids; 852-Charleston, 853-Greenville
+select * from store_locations;  --  Find my terriory storeids; 852-Charleston, 853-Greenville
 
--- What is total revenue overall for sales in the assigned territory, plus the start date and end date
--- that tell you what period the data covers?
+select  								-- Total revenue in both store in South Carolina
+	min(Transaction_Date) as StartDate,
+    max(Transaction_Date) as EndDate,
+    round(sum(Sale_Amount),2) as TotalRevenue
+from 
+	store_sales
+where
+	Store_ID = 852 or Store_ID = 853;
+    
 
-select * from store_sales -- all in store sales for both stores
+select								-- Month by Month Revenue for South Carolina
+	year(Transaction_Date) as Year,
+    month(Transaction_Date) as Month,
+	round(sum(Sale_Amount),2) as MonthlyRevenue
+from
+	store_sales
 where
 	Store_ID = 852 or Store_ID = 853
+group by
+	year(Transaction_Date),
+    month(Transaction_Date)
 order by
-	Transaction_Date asc;
+	year asc;
+    
+
+select									-- Total revenue comparason for South Carolina an the whole South region
+    sum(Sale_Amount) as RegionRevenue,
+	(select
+		sum(Sale_Amount)
+	from 
+			store_sales
+	join 
+		store_locations 
+	on 
+		store_sales.Store_ID = store_locations.StoreId
+	where
+		State = 'South Carolina'
+    ) 
+as TerritoryRevenue
+from
+	store_sales
+join 
+	store_locations 
+on 
+	store_sales.Store_ID = store_locations.StoreId
+where
+	State in('Florida','Texas','South Carolina');
+    
+--  What is the number of transactions per month and average transaction size by product category
+-- for the sales territory?
+    
+  
+
+    
+
+	
+
+
+
+
 
 
 
